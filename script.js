55d6c66bd21f98ccd59381a8d193efd9fdd7c5a4
psycho33200 <pininfarina47@gmail.com>
2019-10-02 10:36:16 +0200

    function for colors

---
+++ script.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.yellow('milan ') + chalk.red('délégué'));
+log(chalk.blue('david ') + chalk.green('bowie'));
+log(chalk.cyan('juliette ') + chalk.grey('boyrie'));
